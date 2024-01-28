e67f8469f868a801dc5c998b1d06612099eb7981
Ryan Baig <86055075+RyanBaig@users.noreply.github.com>
2024-01-28 16:55:43 +0500

    updated with new free mediu article provider + removed a useless restriction

--- popup.js
+++ popup.js
@@ -14,14 +14,10 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("add-url").addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       var currentUrl = tabs[0].url;
-      if (!currentUrl.includes("medium.com")) {
-        alert("Please open a valid Medium.com URL in the current tab");
-        return;
-      } else {
-        var newUrl = "https://medium-parser.vercel.app/?url=";
+        var newUrl = "https://readmedium.com/";
         var modifiedUrl = newUrl + currentUrl;
         chrome.tabs.update(tabs[0].id, { url: modifiedUrl });
-      }
+      
     });
   });
 });
